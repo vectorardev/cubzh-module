bc42be9624df3b5211d2c0689990c7a08a0a4227
Victor Armisen <111573791+vectorardev@users.noreply.github.com>
2024-05-04 18:57:49 +0200

    asdada

--- cubzh-module/module.lua
+++ cubzh-module/module.lua
@@ -8,62 +8,53 @@ cubzhMod.create = function(_)
     print("HOlaaaa")
 end 
 
+
 Client.OnStart = function() 
     -- Read the files of the images in the folder
     images = {}
     --- Setting UI system
     ui = require("uikit")
     --- 
-    -- local inputText = ui:createTextInput("", "Type here", "default")
-    -- local btn = ui:createButton("Send")
-    -- -- btn:setParent(ui.rootFrame)
-    -- btn.Position = Number2(200, 0) 
+    local inputText = ui:createTextInput("", "Type here", "default")
+    local btn = Button("Send")
+    -- btn:setParent(ui.rootFrame)
+    btn.Position = Number2(200, 0) 
     ---
     local urlImages = "https://raw.githubusercontent.com/vectorardev/cubzh-module/master/cubzh-module/inventory-images/"
     local fileType = ".png" 
     local fileName = ""
 
-    local button = ui:createButton("Click here")
-    button:setParent(ui.rootFrame) -- must be set to ui.rootFrame or another ui element
-    button:setColor(Color.Blue, Color.White) -- set background color and text color
-    button.LocalPosition = Number3(Screen.Width - button.Width - 5, 100, 0)
-    button.onRelease = function()
-        print("Click")
+    btn.OnPress = function(_) 
+        print("Helloooooooo") 
+        if inputText._text == "babyJoda" then 
+            fileName = "babyJoda"
+        end
+        if inputText._text == "chiwaka" then 
+            fileName = "chiwaka"
+        end
+        if inputText._text == "luke" then 
+            fileName = "luke"
+        end
+        if inputText._text == "maestro" then 
+            fileName = "maestro"
+        end
+        if inputText._text == "noidea" then 
+            fileName = "noidea"
+        end
+        if inputText._text == "r2d2" then 
+            fileName = "r2d2"
+        end
+        local finalUrl = urlImages .. fileName .. fileType
+        HTTP:Get(finalUrl, function(res)
+            if res.StatusCode == 200 then
+                print("The URL IS GOOD")
+                -- show the content of the image. 
+                print(res.Body)
+            else
+                print("CAN'T FIND THE URL")
+            end
+        end)
     end
-
-    -- btn.OnPress = function(_) 
-    --     print("Helloooooooo") 
-    --     if inputText._text == "babyJoda" then 
-    --         fileName = "babyJoda"
-    --     end
-    --     if inputText._text == "chiwaka" then 
-    --         fileName = "chiwaka"
-    --     end
-    --     if inputText._text == "luke" then 
-    --         fileName = "luke"
-    --     end
-    --     if inputText._text == "maestro" then 
-    --         fileName = "maestro"
-    --     end
-    --     if inputText._text == "noidea" then 
-    --         fileName = "noidea"
-    --     end
-    --     if inputText._text == "r2d2" then 
-    --         fileName = "r2d2"
-    --     end
-    --     local finalUrl = urlImages .. fileName .. fileType
-    --     HTTP:Get(finalUrl, function(res)
-    --         if res.StatusCode == 200 then
-    --             print("The URL IS GOOD")
-    --             -- show the content of the image. 
-    --             print(res.Body)
-    --         else
-    --             print("CAN'T FIND THE URL")
-    --         end
-    --     end)
-    -- end
-    ---
-
 
 end
 
