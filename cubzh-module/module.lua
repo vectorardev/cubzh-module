7ad4f4d0370d8164735800ff6051b50c9025ce58
Victor Armisen <111573791+vectorardev@users.noreply.github.com>
2024-05-04 17:13:59 +0200

    28378sdds

--- cubzh-module/module.lua
+++ cubzh-module/module.lua
@@ -8,4 +8,24 @@ cubzhMod.create = function(_)
     print("HOlaaaa")
 end 
 
+Client.OnStart = function() 
+    File:OpenAndReadAll(function(success, result)
+        -- success is a boolean
+        if not success then
+          print("Could not read file")
+          return
+        end
+      
+        -- the user has cancelled the file selection
+        if result == nil then
+          print("No file selected")
+          return
+        end
+      
+        -- result is a Data instance
+        local str = result:ToString()
+        print("Content: ", str) 
+      end)
+end
+
 return cubzhMod
