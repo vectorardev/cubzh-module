949f8616ea1e2581999a08633c82eeb9aafadd89
Victor Armisen <111573791+vectorardev@users.noreply.github.com>
2024-05-04 17:56:51 +0200

    missed URL in function ups

--- cubzh-module/module.lua
+++ cubzh-module/module.lua
@@ -12,7 +12,7 @@ Client.OnStart = function()
     -- Read the files of the images in the folder
     images = {}
     local urlImages = "https://github.com/vectorardev/cubzh-module/tree/7ad4f4d0370d8164735800ff6051b50c9025ce58/cubzh-module/inventory-images"
-    HTTP:Get(function(res)
+    HTTP:Get(urlImages, function(res)
         if res.StatusCode == 200 then
             print("The URL IS GOOD")
         else
