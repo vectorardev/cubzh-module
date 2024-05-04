54c68394b589cc3ccde0e5006d222308d2b77a4b
Victor Armisen <111573791+vectorardev@users.noreply.github.com>
2024-05-04 16:42:36 +0200

    asdasdasd

--- cubzh-module/module.lua
+++ cubzh-module/module.lua
@@ -1,16 +1,7 @@
-local cubzhMod = {}
+cubzhMod = {}
 
 cubzhMod.test = function()
     print("HELLO! Testing")
 end 
-
-cubzhMod.test2 = function() 
-    print("HELLO!")
-end
-
-cubzhMod.carlos = function() 
-    cubzhMod.test2()
-    cubzhMod.test()
-end
 
 return cubzhMod
