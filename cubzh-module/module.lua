eb9069c9e892e3163acfd711dbd80fbf1965c9d8
Victor Armisen <111573791+vectorardev@users.noreply.github.com>
2024-05-04 18:51:42 +0200

    asdasd

--- cubzh-module/module.lua
+++ cubzh-module/module.lua
@@ -14,10 +14,10 @@ Client.OnStart = function()
     --- Setting UI system
     ui = require("uikit")
     --- 
-    local inputText = ui:createTextInput("", "Type here", "default")
-    local btn = ui:createButton("Send")
-    -- btn:setParent(ui.rootFrame)
-    btn.Position = Number2(200, 0) 
+    -- local inputText = ui:createTextInput("", "Type here", "default")
+    -- local btn = ui:createButton("Send")
+    -- -- btn:setParent(ui.rootFrame)
+    -- btn.Position = Number2(200, 0) 
     ---
     local urlImages = "https://raw.githubusercontent.com/vectorardev/cubzh-module/master/cubzh-module/inventory-images/"
     local fileType = ".png" 
@@ -30,38 +30,38 @@ Client.OnStart = function()
     button.onRelease = function()
         print("Click")
     end
-    
-    btn.OnPress = function(_) 
-        print("Helloooooooo") 
-        if inputText._text == "babyJoda" then 
-            fileName = "babyJoda"
-        end
-        if inputText._text == "chiwaka" then 
-            fileName = "chiwaka"
-        end
-        if inputText._text == "luke" then 
-            fileName = "luke"
-        end
-        if inputText._text == "maestro" then 
-            fileName = "maestro"
-        end
-        if inputText._text == "noidea" then 
-            fileName = "noidea"
-        end
-        if inputText._text == "r2d2" then 
-            fileName = "r2d2"
-        end
-        local finalUrl = urlImages .. fileName .. fileType
-        HTTP:Get(finalUrl, function(res)
-            if res.StatusCode == 200 then
-                print("The URL IS GOOD")
-                -- show the content of the image. 
-                print(res.Body)
-            else
-                print("CAN'T FIND THE URL")
-            end
-        end)
-    end
+
+    -- btn.OnPress = function(_) 
+    --     print("Helloooooooo") 
+    --     if inputText._text == "babyJoda" then 
+    --         fileName = "babyJoda"
+    --     end
+    --     if inputText._text == "chiwaka" then 
+    --         fileName = "chiwaka"
+    --     end
+    --     if inputText._text == "luke" then 
+    --         fileName = "luke"
+    --     end
+    --     if inputText._text == "maestro" then 
+    --         fileName = "maestro"
+    --     end
+    --     if inputText._text == "noidea" then 
+    --         fileName = "noidea"
+    --     end
+    --     if inputText._text == "r2d2" then 
+    --         fileName = "r2d2"
+    --     end
+    --     local finalUrl = urlImages .. fileName .. fileType
+    --     HTTP:Get(finalUrl, function(res)
+    --         if res.StatusCode == 200 then
+    --             print("The URL IS GOOD")
+    --             -- show the content of the image. 
+    --             print(res.Body)
+    --         else
+    --             print("CAN'T FIND THE URL")
+    --         end
+    --     end)
+    -- end
     ---
 
 
