f418096fab15ac38ba0ea5de8f9371d7b60597fa
Victor Armisen <111573791+vectorardev@users.noreply.github.com>
2024-05-14 13:33:56 +0200

    forgot *

--- cubzh-module/module.lua
+++ cubzh-module/module.lua
@@ -123,7 +123,7 @@ inventory_ui.init = function(items)
     local i = 0
     for k,v in pairs(items) do
         for n,r in pairs(v) do
-            itemCount = i + 75
+            itemCount = i * 75
             ---Number3(inventory_ui.bg.Width / 2 + 10, inventory_ui.bg.Height / 2 + 10, 0)
             inventory_ui.item(Color.Green, "nil", n, inventory_ui.bg.Position + Number3(50, itemCount, 0), r)
             i = i + 1
